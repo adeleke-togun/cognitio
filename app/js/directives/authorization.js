d6b4525ed6f65c4830442e2f9b3638e3682d2909
Seun Martins <impactmass2000@gmail.com>
2015-04-28 11:59:30 +0100

    restrict access to pages

--- app/js/directives/authorization.js
+++ app/js/directives/authorization.js
@@ -3,10 +3,8 @@ angular.module('cognitio.directives')
   .directive('isAuthorized', function() {
     return {
       restrict: 'EA',
-      controller: ['$scope', 'Authorization',
-        function($scope, Authorization) {
-        console.log('got here into Authorization');
-        // checks to see if logged in user is an admin use is-authorized ng-show='showPage' on all admin pages
+      controller: ['$scope', 'Authorization', '$location',
+        function($scope, Authorization, $location) {
         Authorization.isAuthorized().then(function(admin) {
           if(!admin) {
             // window.location.pathname = '/login';
@@ -14,6 +12,8 @@ angular.module('cognitio.directives')
           else{
             $scope.showPage = true;
           }
+        }, function (err) {
+          $location.path('/');
         });
       }]
     };
