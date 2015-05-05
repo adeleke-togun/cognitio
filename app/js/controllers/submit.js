43a0633fe827b39afda14cd5a5bf06f488e975a0
andela-ooladayo <oladayo.oyelade@andela.co>
2015-05-05 16:41:33 +0100

    fix angualr file upload

--- app/js/controllers/submit.js
+++ app/js/controllers/submit.js
@@ -1,14 +1,38 @@
 angular.module('cognitio.controllers')
-.controller('SubmitCtrl', ['$scope', 'Assessment', 'Submission', '$location', 
-	function ($scope, Assessment, Submission, $location) {
+.controller('SubmitCtrl', ['$scope', 'Assessment', 'Submission', '$location', 'Upload', 
+	function ($scope, Assessment, Submission, $location, Upload) {
 
   //retrieve all assessments
   $scope.assessments = Assessment.all();
 
   //grab form data and submit assessments
   $scope.send = function (response) {
-    Submission.submit(response.assessment_id, response);
-    $location.path('/home');
-    toastr.success("Your assignment was submitted successfully!");
+    var fil = $scope.files[0]
+    var filename = fil.name;
+    Upload.upload({
+      url: 'https://studentrent-bucket.s3-us-west-2.amazonaws.com/',
+      file: fil,
+      fileName: filename,
+      method: 'POST',
+      fields : {
+        key: filename,
+        AWSAccessKeyId: 'AKIAISEQHWBOLTLYDFYQ', 
+        acl: 'private',
+        policy: 'ewogICJleHBpcmF0aW9uIjogIjIwMjAtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsKICAgIHsiYnVja2V0IjogInN0dWRlbnRyZW50LWJ1Y2tldCJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIka2V5IiwgIiJdLAogICAgeyJhY2wiOiAicHJpdmF0ZSJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogICAgWyJzdGFydHMtd2l0aCIsICIkZmlsZW5hbWUiLCAiIl0sCiAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogIF0KfQ==',
+        signature: 'g/OPVsj8f1k5XsFEZ8BkSZDDRIc=',
+        'Content-Type' : fil.type === null || fil.type === '' ? 'application/octet-stream' : fil.type,
+        filename: filename
+      },
+
+   }).success(function() {
+      response.file = filename;
+      Submission.submit(response.assessment_id, response)
+      toastr.success("Your assignment was submitted successfully!");
+      $location.path('/home');
+    }).error(function(){
+      toastr.info('Failed to uploaded to S3');
+   });
+ 
   };
+
 }]);
--- app/js/services/submission.js
+++ app/js/services/submission.js
@@ -7,17 +7,16 @@ angular.module('cognitio.services')
         },
         submit: function(assessment_id, assessment) {
           console.log($rootScope.currentUser)
-          var uid = $rootScope.currentUser.uid
-          var name = $rootScope.currentUser.name
-          var email = $rootScope.currentUser.email
+          var uid = $rootScope.currentUser.uid;
+          var name = $rootScope.currentUser.name;
+          var email = $rootScope.currentUser.email;
 
-          var assessmentObject = {}
-          assessmentObject.submitted_at = Firebase.ServerValue.TIMESTAMP
-          assessmentObject.submitted_by = name
-          assessmentObject.submitted_by_email = email
-          assessmentObject.description = assessment.description
-          // assessmentObject.file_url = assessment.fileUploaded
-
+          var assessmentObject = {};
+          assessmentObject.submitted_at = Firebase.ServerValue.TIMESTAMP;
+          assessmentObject.submitted_by = name;
+          assessmentObject.submitted_by_email = email;
+          assessmentObject.description = assessment.description;
+          assessmentObject.file_url = assessment.file;
           return Refs.submissions.child(assessment_id).child(uid).set(assessmentObject)
         },
         getAllAssessmentSubmission: function(assessment_id) {
