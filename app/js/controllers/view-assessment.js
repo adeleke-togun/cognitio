85c87fe8374565173a6ce6bf3cfd762e1df92abc
Andela Dev <olaide.agboola@andela.co>
2015-05-06 16:03:39 +0100

    refactored code

--- app/js/controllers/view-assessment.js
+++ app/js/controllers/view-assessment.js
@@ -2,14 +2,10 @@ angular.module('cognitio.controllers')
 .controller('viewAssesmentCtrl',
   ['$scope', '$state', 'Authentication', 'Assessment',
     function($scope, $state, Authentication, Assessment) {
-      $scope.assessments = Assessment.all();
-      console.log("assessments", $scope.assessments);
+      var assessments = Assessment.all();
       
-      for(var i = 0; i < $scope.assessments.length; i++) {
-        console.log("jjjj", $scope.assessments[i]);
-        $scope.createdDate = new Date($scope.assessments[i].created_at * 1000);
-        console.log("converted date", $scope.createdDate);
-      }
-      // angular.forEach(function() {});
+      assessments.$loaded(function(res) {
+        $scope.assessments = res;
+      });
     }
  ]);
