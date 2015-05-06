f00def4f30a989b590fc030b04dcd4221ed5d052
andela-ooladayo <oladayo.oyelade@andela.co>
2015-05-06 20:06:13 +0100

    fixing bug

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function run(appdir, rootRef) {
   app.use(cookieParser());
 
   app.dir = appdir;
-  console.log('app dir---------->',appdir);
 
   // things to do on each request
   app.use(function (req, res, next) {
